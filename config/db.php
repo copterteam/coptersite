7ab0212e0c59386a7e17f8384999852ab85cf040
copterteam <rucopterteam@gmail.com>
2017-02-23 00:18:17 +0300

    DB

--- config/db.php
+++ config/db.php
@@ -1,9 +1,23 @@
 <?php
-
+if (YII_ENV_DEV) {
+	
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=copterteam',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
+];	
+	
+	
+}else{
+	
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=u61668_copterteam',
+    'username' => 'u61668',
+    'password' => 'tip9tnxyw4',
+    'charset' => 'utf8',
 ];
+
+}
