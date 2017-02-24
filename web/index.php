624c772e2c6dd563c421457fb12efe4b1d8245fe
copterteam <rucopterteam@gmail.com>
2017-02-24 13:11:42 +0300

    get_env

--- web/index.php
+++ web/index.php
@@ -4,7 +4,11 @@ use yii\helpers\ArrayHelper;
 
 
 // comment out the following two lines when deployed to production
-defined('YII_ENV') or define('YII_ENV', 'dev');
+//defined('YII_ENV') or define('YII_ENV', 'dev');
+
+
+defined('YII_ENV') or define('YII_ENV', getenv('APPLICATION_ENV') ?: 'prod');
+
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
